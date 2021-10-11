5126f68840a4849082bd9247052f68c74f46c2de
techchee <ketyung@techchee.com>
2021-10-11 10:30:32 +0800

    update

--- src/processor.rs
+++ src/processor.rs
@@ -192,3 +192,77 @@ fn mint_token(program_id: &Pubkey, accounts: &[AccountInfo],token_count : u64 )-
 }
 
 
+
+/*
+fn process_exchange(
+    accounts: &[AccountInfo],
+    amount_expected_by_taker: u64,
+    program_id: &Pubkey,
+    
+) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+    let taker = next_account_info(account_info_iter)?;
+
+    if !taker.is_signer {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let takers_sending_token_account = next_account_info(account_info_iter)?;
+
+    let takers_token_to_receive_account = next_account_info(account_info_iter)?;
+
+    let pdas_temp_token_account = next_account_info(account_info_iter)?;
+    
+    let (pda, bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
+
+    
+    let initializers_main_account = next_account_info(account_info_iter)?;
+    let initializers_token_to_receive_account = next_account_info(account_info_iter)?;
+    let escrow_account = next_account_info(account_info_iter)?;
+
+    
+    let token_program = next_account_info(account_info_iter)?;
+
+    let transfer_to_initializer_ix = spl_token::instruction::transfer(
+        token_program.key,
+        takers_sending_token_account.key,
+        initializers_token_to_receive_account.key,
+        taker.key,
+        &[&taker.key],
+        amount_expected_by_taker,
+    )?;
+    msg!("Calling the token program to transfer tokens to the escrow's initializer...");
+    invoke(
+        &transfer_to_initializer_ix,
+        &[
+            takers_sending_token_account.clone(),
+            initializers_token_to_receive_account.clone(),
+            taker.clone(),
+            token_program.clone(),
+        ],
+    )?;
+    Ok(())
+}
+
+
+let transfer_to_taker_ix = spl_token::instruction::transfer(
+    token_program.key,
+    pdas_temp_token_account.key,
+    takers_token_to_receive_account.key,
+    &pda,
+    &[&pda],
+    pdas_temp_token_account_info.amount,
+)?;
+msg!("Calling the token program to transfer tokens to the taker...");
+invoke_signed(
+    &transfer_to_taker_ix,
+    &[
+        pdas_temp_token_account.clone(),
+        takers_token_to_receive_account.clone(),
+        pda_account.clone(),
+        token_program.clone(),
+    ],
+    &[&[&b"escrow"[..], &[bump_seed]]],
+)?;
+
+*/
